dc77a7059b888ee1ed27420aa5c6b5a9a678a363
Hyukjin Kwon <hjnim0909@gmail.com>
2020-04-18 11:49:09 +0900

    Done with Chapter3

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fielsets = UserAdmin.fielsets + (
+        "Custom Profile",
+        {
+            "fields": (
+                "avatar",
+                "gender",
+                "bio",
+                "birthday",
+                "language",
+                "currency",
+                "superhost",
+            )
+        },
+    )
